08246f399b1b9cbcc6536a171eb6787e6f47cdf7
riku-miyagi <ookiddoairis@gmail.com>
2024-03-17 08:17:17 +0900

    Update：ロケールの設定を記載

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
+require 'csv'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -23,5 +24,10 @@ module KadaiMiyagi
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # タイムゾーンを日本時間に設定
+    config.time_zone = 'Asia/Tokyo'
+    # デフォルトのロケールを日本(ja)に設定
+    config.i18n.default_locale = :ja
   end
 end
