ce828331bce1f9b48bf736c1d96bde2b0cdffb11
riku-miyagi <ookiddoairis@gmail.com>
2024-03-17 13:25:24 +0900

    update：BlogにCSVインポートするための新たなアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :blogs
+  resources :blogs do
+    collection { post :import }
+  end
+
   root 'blogs#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
