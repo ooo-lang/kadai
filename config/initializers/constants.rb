c736e76ab9c49ea4f1c808df393a83047c8d4aea
riku-miyagi <ookiddoairis@gmail.com>
2024-03-17 18:19:52 +0900

    add：定数ファイルを追加

---
+++ config/initializers/constants.rb
@@ -0,0 +1,10 @@
+# Blogモデルで使用する定数を定義
+module BlogModelConstants
+
+  # ヘッダー名を定義
+  HEADER_BLOG_TITLE = 'blog_title'
+  HEADER_CONTENT = 'blog_content'
+  HEADER_CATEGORY = 'categories'
+  HEADER_CREATED_AT = 'created_at'
+  HEADER_UPDATED_AT = 'updated_at'
+end
