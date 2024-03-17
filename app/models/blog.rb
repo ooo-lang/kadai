34ecb329f5d4c3ea6b1368358413916dc2bd5497
riku-miyagi <ookiddoairis@gmail.com>
2024-03-17 18:20:43 +0900

    update：コメント追加、CSVヘッダー名を定数化

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,20 +2,24 @@ class Blog < ApplicationRecord
   has_many :categorizations
   has_many :categories, through: :categorizations
 
+  # CSデータ読込処理
+  #
+  # @param file [string] CSVデータのファイルパス
+  # @return void
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
-      created_at = Time.zone.parse(row['created_at'])
-      updated_at = Time.zone.parse(row['updated_at'])
-
       blog = Blog.new(
-        title: row['blog_title'],
-        content: row['blog_content'],
-        created_at: created_at,
-        updated_at: updated_at
+        title: row[BlogModelConstants::HEADER_BLOG_TITLE],
+        content: row[BlogModelConstants::HEADER_CONTENT],
+        created_at: Time.zone.parse(row[BlogModelConstants::HEADER_CREATED_AT]),
+        updated_at: Time.zone.parse(row[BlogModelConstants::HEADER_UPDATED_AT])
       )
 
-      categories = row['categories'].split(',').map(&:strip)
+      # カテゴリをカンマ区切りで配列化
+      categories = row[BlogModelConstants::HEADER_CATEGORY].split(',').map(&:strip)
+
       categories.each do |category_name|
+        # 既存のカテゴリが無ければ生成
         category = Category.find_or_create_by(name: category_name)
         blog.categories << category
       end
