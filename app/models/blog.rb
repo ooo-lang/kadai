c748542e3c3a0b3e84e43daed450242503159f1f
riku-miyagi <ookiddoairis@gmail.com>
2024-03-17 13:21:18 +0900

    fix：CSVの日時データが日本時間に修正されて表示されるバグ
    Time.zone.parseを使用することで、日本時間としてCSVデータを扱ってそのまま生成されるように修正

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,26 @@
 class Blog < ApplicationRecord
   has_many :categorizations
   has_many :categories, through: :categorizations
+
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      created_at = Time.zone.parse(row['created_at'])
+      updated_at = Time.zone.parse(row['updated_at'])
+
+      blog = Blog.new(
+        title: row['blog_title'],
+        content: row['blog_content'],
+        created_at: created_at,
+        updated_at: updated_at
+      )
+
+      categories = row['categories'].split(',').map(&:strip)
+      categories.each do |category_name|
+        category = Category.find_or_create_by(name: category_name)
+        blog.categories << category
+      end
+
+      blog.save
+    end 
+  end
 end
